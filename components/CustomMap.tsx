b95a229e365cb1706bfd19405bc708df35cf4101
JunasVee <arjunagamers777@gmail.com>
2025-03-24 21:01:30 +0700

    first commit, empty project

---
+++ components/CustomMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CustomMap() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Calendar, Home, Inbox, Settings, Phone } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Assignments",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "History",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Contact Admin",
+    url: "#",
+    icon: Phone,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
